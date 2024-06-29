c912585eb1a3fb6592e60a808de9592e8f3b0d5b
Bayomi Aremo <8955yomi@gmail.com>
2024-06-29 12:31:28 -0700

    boilerplate added

---
+++ src/app/Navbar.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+export default function Navbar() {
+  return (
+    <div className="w-[90%] mx-auto">
+        <ul className="mx-auto">
+            <li>About</li>
+            <li>Product</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+  )
+}
---
+++ src/app/PageWrapper.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+export default function PageWrapper(children: any) {
+  return (
+    <div>{children}</div>
+  )
+}
